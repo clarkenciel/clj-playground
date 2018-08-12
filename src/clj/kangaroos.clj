66b862cb7aac5b162ae0f72a73a82eb6c9d2eda8
clarkenciel <clarkenciel@gmail.com>
2018-08-12 10:18:40 -0700

    remove "names" for terms to avoid potential runtime errors via if short-circuit

--- src/clj/kangaroos.clj
+++ src/clj/kangaroos.clj
@@ -1,8 +1,7 @@
 (ns kangaroos)
 
 (defn kangaroo [x1 v1 x2 v2]
-  (let [can-catch-up? (< v2 v1)
-        will-sync? (= 0 (mod (- x2 x1) (- v1 v2)))]
-    (if (and can-catch-up? will-sync?)
-      "YES"
-      "NO")))
+  (if (and  (< v2 v1)
+        (= 0 (mod (- x2 x1) (- v1 v2))))
+    "YES"
+    "NO"))
